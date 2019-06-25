b55277e1ada7eba8f0956cc5eac46d6ca65d0673
jwkeena <jwkeena@gmail.com>
2019-06-25 16:02:53 -0400

    More profiles, enabled modal, full stack cycle

--- app/data/singles.js
+++ app/data/singles.js
@@ -1,163 +1,331 @@
 const singles = [{
-    "name": "Bruce Wayne",
-    "photo": "https://vignette.wikia.nocookie.net/batmanfanon/images/5/54/Btmancbale.png/revision/latest?cb=20130910015942",
-    "surveyAnswers": [
-        "5",
-        "5",
-        "3",
-        "3",
-        "5",
-        "3",
-        "2",
-        "5",
-        "3",
-        "1",
-        "4"
-    ]
-}, {
-    "name": "Clark Kent",
-    "photo": "https://hips.hearstapps.com/esq.h-cdn.co/assets/15/17/1429931714-man-of-steel-superman.jpg?resize=480:*",
-    "surveyAnswers": [
-        "2",
-        "2",
-        "5",
-        "5",
-        "3",
-        "2",
-        "5",
-        "3",
-        "4",
-        "3",
-        "4"
-    ]
-}, {
-    "name": "Carol Danvers",
-    "photo": "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2019/02/mfm1160_cmp_v002.1031_r.jpg",
-    "surveyAnswers": [
-        "2",
-        "2",
-        "1",
-        "2",
-        "1",
-        "1",
-        "1",
-        "2",
-        "2",
-        "3",
-        "5"
-    ]
-}, {
-    "name": "Logan",
-    "photo": "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/06/06/Pictures/fxm005al_68ee4626-8837-11e9-8812-e0f6aa62d95d.jpg",
-    "surveyAnswers": [
-        "4",
-        "4",
-        "5",
-        "1",
-        "1",
-        "2",
-        "1",
-        "1",
-        "1",
-        "1",
-        "2"
-    ]
-}, {
-    "name": "Diana Prince",
-    "photo": "https://www.indiewire.com/wp-content/uploads/2017/12/wonder-woman-759.jpg?w=759",
-    "surveyAnswers": [
-        "1",
-        "1",
-        "5",
-        "4",
-        "2",
-        "2",
-        "5",
-        "3",
-        "4",
-        "3",
-        "5"
-    ]
-}, {
-    "name": "Peter Quill",
-    "photo": "https://boundingintocomics.com/files/2018/12/Chris-Pratt.png",
-    "surveyAnswers": [
-        "5",
-        "5",
-        "3",
-        "3",
-        "1",
-        "1",
-        "5",
-        "2",
-        "3",
-        "5",
-        "4"
-    ]
-}, {
-    "name": "Tony Stark",
-    "photo": "https://cdn.vox-cdn.com/thumbor/MnJIKau4IBkdWn6FiJff9nzmZdw=/127x0:1848x1147/1200x800/filters:focal(127x0:1848x1147)/cdn.vox-cdn.com/uploads/chorus_image/image/41985826/iron_man_and_rdj.0.0.jpeg",
-    "surveyAnswers": [
-        "3",
-        "3",
-        "1",
-        "3",
-        "1",
-        "1",
-        "3",
-        "2",
-        "2",
-        "2",
-        "3"
-    ]
-}, {
-    "name": "Raven",
-    "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x-men-dark-phoenix-1551279498.jpg",
-    "surveyAnswers": [
-        "5",
-        "5",
-        "5",
-        "2",
-        "4",
-        "5",
-        "5",
-        "3",
-        "4",
-        "1",
-        "3"
-    ]
-}, {
-    "name": "Erik Lehnsherr",
-    "photo": "https://img.cinemablend.com/filter:scale/quill/2/3/a/f/3/6/23af36b56f10865fbce8963372c2cc24dc303f0e.jpg?mw=600",
-    "surveyAnswers": [
-        "5",
-        "5",
-        "5",
-        "2",
-        "3",
-        "1",
-        "5",
-        "4",
-        "1",
-        "1",
-        "2"
-    ]
-}, {
-    "name": "Natasha Romanov",
-    "photo": "https://amp.insider.com/images/5c8a8141dd08611286199063-750-563.jpg",
-    "surveyAnswers": [
-        "5",
-        "5",
-        "1",
-        "2",
-        "4",
-        "2",
-        "3",
-        "3",
-        "5",
-        "1",
-        "3"
-    ]
-}]
+        "name": "Bruce Wayne",
+        "photo": "https://vignette.wikia.nocookie.net/batmanfanon/images/5/54/Btmancbale.png/revision/latest?cb=20130910015942",
+        "message": "He says: Get ready for some late night conversations. We've got a lot to share about our traumatic childhoods, theories of justice, and training routines.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "3",
+            "3",
+            "5",
+            "3",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    }, {
+        "name": "Clark Kent",
+        "photo": "https://hips.hearstapps.com/esq.h-cdn.co/assets/15/17/1429931714-man-of-steel-superman.jpg?resize=480:*",
+        "message": "Clark enjoys long walks on the beach, candlelit dinners, and all-American values. He's got the brain of a Boy Scout and the body of Channing Tatum. Lucky you!",
+        "surveyAnswers": [
+            "2",
+            "2",
+            "5",
+            "5",
+            "3",
+            "2",
+            "5",
+            "3",
+            "4",
+            "3",
+            "4"
+        ]
+    }, {
+        "name": "Carol Danvers",
+        "photo": "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2019/02/mfm1160_cmp_v002.1031_r.jpg",
+        "message": "She says: Last time I checked, I was out of your league. Leave a message on my pager and I'll get back to you. Maybe.",
+        "surveyAnswers": [
+            "2",
+            "2",
+            "1",
+            "2",
+            "1",
+            "1",
+            "1",
+            "2",
+            "2",
+            "3",
+            "5"
+        ]
+    }, {
+        "name": "Logan",
+        "photo": "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/06/06/Pictures/fxm005al_68ee4626-8837-11e9-8812-e0f6aa62d95d.jpg",
+        "message": "He says: I'm too old for this shit. Anyone know Jean's phone number?",
+        "surveyAnswers": [
+            "4",
+            "4",
+            "5",
+            "1",
+            "1",
+            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "2"
+        ]
+    }, {
+        "name": "Diana Prince",
+        "photo": "https://www.indiewire.com/wp-content/uploads/2017/12/wonder-woman-759.jpg?w=759",
+        "message": "She says: Didn't know men existed till I was 18. Looking for some education.",
+        "surveyAnswers": [
+            "1",
+            "1",
+            "5",
+            "4",
+            "2",
+            "2",
+            "5",
+            "3",
+            "4",
+            "3",
+            "5"
+        ]
+    }, {
+        "name": "Peter Quill",
+        "photo": "https://boundingintocomics.com/files/2018/12/Chris-Pratt.png",
+        "message": "He says: I've got a lot of these dating profiles, so hit me up on social media to make sure I get back to you.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "3",
+            "3",
+            "1",
+            "1",
+            "5",
+            "2",
+            "3",
+            "5",
+            "4"
+        ]
+    }, {
+        "name": "Tony Stark",
+        "photo": "https://cdn.vox-cdn.com/thumbor/MnJIKau4IBkdWn6FiJff9nzmZdw=/127x0:1848x1147/1200x800/filters:focal(127x0:1848x1147)/cdn.vox-cdn.com/uploads/chorus_image/image/41985826/iron_man_and_rdj.0.0.jpeg",
+        "message": "He says: I'm always wearing a suit, so if you can't handle that, I don't think I'm your type. P.S. Don't tell Pepper I'm on here. ",
+        "surveyAnswers": [
+            "3",
+            "3",
+            "1",
+            "3",
+            "1",
+            "1",
+            "3",
+            "2",
+            "2",
+            "2",
+            "3"
+        ]
+    }, {
+        "name": "Raven",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x-men-dark-phoenix-1551279498.jpg",
+        "message": "She says: I'm adaptible.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "5",
+            "3",
+            "4",
+            "1",
+            "3"
+        ]
+    }, {
+        "name": "Erik Lehnsherr",
+        "photo": "https://img.cinemablend.com/filter:scale/quill/2/3/a/f/3/6/23af36b56f10865fbce8963372c2cc24dc303f0e.jpg?mw=600",
+        "message": "He says: Loner looking for love in all the wrong places. Tired of all the drama with my ex Charles.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "5",
+            "2",
+            "3",
+            "1",
+            "5",
+            "4",
+            "1",
+            "1",
+            "2"
+        ]
+    }, {
+        "name": "Natasha Romanov",
+        "photo": "https://amp.insider.com/images/5c8a8141dd08611286199063-750-563.jpg",
+        "message": "She says: I've been on a few missions already, if you know what I mean. Also, I have a lot of emotional baggage. Get set for a good cry over dinner.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "1",
+            "2",
+            "4",
+            "2",
+            "3",
+            "3",
+            "5",
+            "1",
+            "3"
+        ]
+    }, {
+        "name": "Steve Rogers",
+        "photo": "https://img.cinemablend.com/filter:scale/quill/6/2/7/a/b/2/627ab2159611e7a42ace90e953c06517840430af.jpg?mw=600",
+        "message": "He says: If you remind me of Peggy, I'll be happy.",
+        "surveyAnswers": [
+            "2",
+            "2",
+            "1",
+            "5",
+            "5",
+            "1",
+            "5",
+            "4",
+            "5",
+            "3",
+            "3"
+        ]
+    }, {
+        "name": "Charles Xavier",
+        "photo": "https://static.tvtropes.org/pmwiki/pub/images/charlesxavier.jpg",
+        "message": "He says: Get ready to have the most honest conversation of your life.",
+        "surveyAnswers": [
+            "1",
+            "1",
+            "5",
+            "5",
+            "3",
+            "2",
+            "5",
+            "4",
+            "5",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Gamora",
+        "photo": "https://i.ytimg.com/vi/ZtJwdtAaO2s/maxresdefault.jpg",
+        "message": "She says: none of this 'dinner and a movie' bs. Take me on a 100-mile bicycle ride or mountain-climbing or martial arts training.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "4",
+            "3",
+            "3",
+            "1",
+            "5",
+            "4",
+            "3",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "The Ancient One",
+        "photo": "https://www.screengeek.net/wp-content/uploads/2019/05/avengers-endgame-tilda-swinton-ancient-one.jpg",
+        "message": "She says: I'm old, but experienced.",
+        "surveyAnswers": [
+            "3",
+            "3",
+            "3",
+            "4",
+            "2",
+            "1",
+            "5",
+            "3",
+            "3",
+            "1",
+            "3"
+        ]
+    },
+    {
+        "name": "Thanos",
+        "photo": "https://i.ytimg.com/vi/N2YTmooNR8E/maxresdefault.jpg",
+        "message": "Sorry. You must be human garbage.",
+        "surveyAnswers": [
+            "4",
+            "4",
+            "4",
+            "2",
+            "5",
+            "1",
+            "2",
+            "5",
+            "1",
+            "1",
+            "3"
+        ]
+    }, {
+        "name": "Arthur Curry",
+        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--xWW1LELb--/c_scale,f_auto,fl_progressive,q_80,w_800/k34p89dvjcsc6o38z6jx.jpg",
+        "message": "He says: Likes: swimming, redheads, avoiding one's destiny. Dislikes: family drama, Bruce Wayne, seafood for dinner.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "5",
+            "2",
+            "2",
+            "4",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3"
+        ]
+    },
+    {
+        "name": "Elastigirl",
+        "photo": "https://vignette.wikia.nocookie.net/disney/images/3/3a/Profile_-_Helen_Parr.jpeg/revision/latest?cb=20190313160047",
+        "message": "She says: Looking for a partner who understands my line of work and is a good listener. Fair warning: get ready to hear about my 'incredible' divorce.",
+        "surveyAnswers": [
+            "1",
+            "1",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5",
+            "3",
+            "4",
+            "3",
+            "5"
+        ]
+    }, {
+        "name": "Peter Parker",
+        "photo": "https://t00.deviantart.net/Qh6QD5jzDksY0Dckd_WMKf-vmR8=/fit-in/300x900/filters:no_upscale():origin()/pre00/c166/th/pre/f/2016/360/3/5/realization__peter_parker_x_reader__by_eightsocks-da1logh.jpg",
+        "message": "He's a little clingy, but friendly too.",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "1",
+            "4",
+            "4",
+            "3",
+            "4",
+            "2",
+            "4",
+            "2",
+            "5"
+        ]
+    }, {
+        "name": "Jean Grey",
+        "photo": "https://ewedit.files.wordpress.com/2019/04/dp_2374y398yr349u9344.jpg",
+        "message": "In the words of the Office, she's a 'Hot and juicy redhead.'",
+        "surveyAnswers": [
+            "5",
+            "5",
+            "5",
+            "3",
+            "3",
+            "5",
+            "4",
+            "3",
+            "3",
+            "4",
+            "3"
+        ]
+    }
+]
 
 module.exports = singles;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,44 @@ module.exports = function (app) {
     });
 
     app.post("/api/singles", function (req, res) {
-        singles.push(req.body);
-        return res.json("Profile successfully stored.")
+        
+        let userProfile = req.body // Grab new user profile from express 
+        let compatibilityScoresArr = [];
+
+        for (let i = 0; i<singles.length; i++) {
+
+            let potentialMatchAnswers = singles[i].surveyAnswers;
+            let userAnswers = userProfile.surveyAnswers;
+            let comparedAnswers = [];
+
+            for (let j=0; j<userAnswers.length; j++) {
+                // Compare the absolute value difference between each survey answer
+                let difference = Math.abs(parseInt(userAnswers[j]) - parseInt(potentialMatchAnswers[j]))
+                comparedAnswers.push(difference);
+            }
+            
+            let compatibilityScore = comparedAnswers.reduce((a, b) => a + b, 0) // Add all numbers in the comparedAnswers array to determine compatibility score
+            console.log("compatibilityScore: " + compatibilityScore + " - for " + singles[i].name)
+            compatibilityScoresArr.push(compatibilityScore); // Store the score in an array for later access
+        }
+
+        // By now, a compatibility score is generated for each profile. 
+        // So, now we find the index position of the lowest compatibility score (this is golf; the lower the score, the more compatible)
+        // Loop through all the compatibility scores to check which is lowest
+        let index = 0;
+        let temp = compatibilityScoresArr[0];
+        for (let k = 0; k<compatibilityScoresArr.length; k++) {
+            if (compatibilityScoresArr[k] < temp) {
+                temp = compatibilityScoresArr[k];
+                index = k;
+            }
+        }
+
+        lowestCompatibilityScore = temp;
+        console.log("lowestCompatibilityScore: " + lowestCompatibilityScore);
+        singles.push(userProfile); // Store new profile, AFTER checking for a match. Otherwise this will be the match
+        return res.json(singles[index]) // This must be the matching profile (note this will resolve in favor of a match later in the singles array if there is a tie
+        
     })
 
 }
